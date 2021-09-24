7e35c64236d32af875abd2d955e8a897cf7e9fdd
Mario <developer@boywhoroared.com>
2021-09-24 09:45:55 -0400

    wip: only partially apply the fn if all args were not provided

--- bfe.dev/curry/index.js
+++ bfe.dev/curry/index.js
@@ -4,8 +4,14 @@ exports.join = (a, b, c) => {
 
 function curry (fn) {
   return function (...x) {
+
+    // handles the case where all arguments are given
+    if (x.length == fn.length) {
+      return fn.apply(null, x)
+    }
+
+    // ...if not, we allow partial application for *all* the remaining arguments (currying)
     return function (...y) {
-      console.log(x, y)
       return fn.apply(null, x.concat(y))
     }
   } 
