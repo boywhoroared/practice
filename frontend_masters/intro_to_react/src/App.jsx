aa13ae5df9c1f0e746c8366b49073366b86641b0
Mario <developer@theboywhoroared.com>
2025-01-16 15:39:09 -0400

    Add API/Static file server; Add images
    This is separately running local server that Vite will be proxying to get the
    API and static assets

--- frontend_masters/intro_to_react/src/App.jsx
+++ frontend_masters/intro_to_react/src/App.jsx
@@ -1,15 +1,27 @@
-import { createRoot } from "react-dom/client"
-import Pizza from "./Pizza"
+import { createRoot } from "react-dom/client";
+import Pizza from "./Pizza";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Pepperoni Pizza" description="Mozzarella Cheese, Pepperoni"></Pizza>
-      <Pizza name="Americano" description="French Fries and Hot Dogs"></Pizza>
-      <Pizza name="The Big Meat Pizza" description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"></Pizza>
+      <Pizza
+        name="Pepperoni Pizza"
+        description="Mozzarella Cheese, Pepperoni"
+        image="/public/pizzas/pepperoni.webp"
+      />
+      <Pizza
+        name="Americano"
+        description="French Fries and Hot Dogs"
+        image="/public/pizzas/bbq_ckn.webp"
+      />
+      <Pizza
+        name="The Big Meat Pizza"
+        description="Bacon, Pepperoni, Italian Sausage, Chorizo Sausage"
+        image="/public/pizzas/big_meat.webp"
+      />
     </div>
-  )
+  );
 };
 
 // Find the empty element to use as the root of the React tree
@@ -17,7 +29,7 @@ const container = document.getElementById("root");
 // Create the root
 const root = createRoot(container);
 
-// Render our application component 
+// Render our application component
 
 // Notice we don't have to do import React from
 // 'react' here like we used to.  The latest version of JSX handles that for you
