dfbed6f91d0aaf71dcdb57813f175fb3077eb007
Mario <developer@theboywhoroared.com>
2025-01-30 23:03:07 -0400

    GS Ch5 update shipping icons easier to test
    Now that update_shipping_icons has a Cart parameter,
    we no longer need to use the global shopping cart
    to run the tests.
    
    It's now easier to use and test.

--- functional_programming/grokking_simplicity/exercises/mega-mart.test.ts
+++ functional_programming/grokking_simplicity/exercises/mega-mart.test.ts
@@ -1,5 +1,12 @@
 import { expect, test, describe, vi } from "vitest";
-import { add_item, gets_free_shipping, buy_buttons, get_buy_buttons_dom, update_shipping_icons, shopping_cart } from "./mega-mart";
+import {
+  add_item,
+  gets_free_shipping,
+  buy_buttons,
+  get_buy_buttons_dom,
+  update_shipping_icons,
+  shopping_cart,
+} from "./mega-mart";
 import type { Cart, CartItem } from "./mega-mart";
 
 describe("Chapter 5", () => {
@@ -17,26 +24,25 @@ describe("Chapter 5", () => {
 
   test("updates shipping icons", () => {
     // setup
-   shopping_cart.push(
+    const cart = [
       { name: "Book", price: 10.0 },
       { name: "Pen", price: 2.0 },
-   )  
+    ];
 
-    buy_buttons.forEach(b => {
-      b.hide_free_shipping_icon = vi.fn(b.hide_free_shipping_icon)
-      b.show_free_shipping_icon = vi.fn(b.show_free_shipping_icon)
-    })
+    buy_buttons.forEach((b) => {
+      b.hide_free_shipping_icon = vi.fn(b.hide_free_shipping_icon);
+      b.show_free_shipping_icon = vi.fn(b.show_free_shipping_icon);
+    });
 
+    update_shipping_icons(cart);
 
-    update_shipping_icons(shopping_cart);
+    expect(buy_buttons[0].hide_free_shipping_icon).not.toHaveBeenCalled();
+    expect(buy_buttons[0].show_free_shipping_icon).toHaveBeenCalled();
 
-    expect(buy_buttons[0].hide_free_shipping_icon).not.toHaveBeenCalled()
-    expect(buy_buttons[0].show_free_shipping_icon).toHaveBeenCalled()
+    expect(buy_buttons[1].hide_free_shipping_icon).not.toHaveBeenCalled();
+    expect(buy_buttons[1].show_free_shipping_icon).toHaveBeenCalled();
 
-    expect(buy_buttons[1].hide_free_shipping_icon).not.toHaveBeenCalled()
-    expect(buy_buttons[1].show_free_shipping_icon).toHaveBeenCalled()
-
-    expect(buy_buttons[2].hide_free_shipping_icon).toHaveBeenCalled()
-    expect(buy_buttons[2].show_free_shipping_icon).not.toHaveBeenCalled()
-  })
+    expect(buy_buttons[2].hide_free_shipping_icon).toHaveBeenCalled();
+    expect(buy_buttons[2].show_free_shipping_icon).not.toHaveBeenCalled();
+  });
 });
